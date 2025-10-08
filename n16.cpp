8e3e474af7025ebb17324c37515ee8dec63e776b
morozov <81422715+fracex@users.noreply.github.com>
2025-10-08 19:26:24 +0300

    Add files via upload

---
+++ n16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main ()
+
+{
+    int t; int v; int x; int y;
+    cin>>t; cin>>v; cin>> x; cin>> y;
+    cout<<"a) 5 10"<<"\n";cout<<"   7 cm"<<endl;
+    cout<<"b) 100 t"<<"\n";cout<<"   1949 v"<<endl;
+    cout<<"в) x 25"<<"\n";cout<<"   x y"<<endl;
+}
