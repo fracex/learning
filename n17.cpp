9e4eb2805b60e01f251210fc958d3cc6b4096c98
morozov <81422715+fracex@users.noreply.github.com>
2025-10-08 19:41:12 +0300

    Add files via upload

---
+++ n17.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main ()
+
+{
+    int a; int b; int x; int y;
+    cin>>a; cin>>b; cin>> x; cin>> y;
+    cout<<"a) 2 кг"<<"\n";cout<<"   13 17"<<endl;
+    cout<<"b) "<<a<<" 1"<<"\n";cout<<"   19 "<<b<<endl;
+    cout<<"в) "<<x<<" "<<y<<"\n";cout<<"   5 "<<y<<endl;
+}
