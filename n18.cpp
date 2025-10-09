4a121f64a0f7a0a7550815c1afaccbcb458e80a6
morozov <81422715+fracex@users.noreply.github.com>
2025-10-09 12:10:40 +0300

    Add files via upload

---
+++ n18.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+    int x, a;
+    cout<<"Введите x:";
+    cin>>x; cout<<" y= "<<(17*x*x)-(6*x)+13<<endl<<"\n";
+    cout<<"Введите a:";
+    cin>>a; cout << "y= "<<(3*a*a)+(5*a)-21<<endl<<"\n";
+}
