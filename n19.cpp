15d2c6b2f2f2d93a90dc1811b628e148086b52bd
morozov <81422715+fracex@users.noreply.github.com>
2025-11-03 22:15:12 +0300

    Add files via upload

---
+++ n19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+
+{
+    int a;
+    cout<<"Введите число:"<<endl;
+    cin>>a;
+    cout<<((a*a)+10)/sqrt((a*a)+1)<<endl;
+}
