5918d4969404ab7c0cf0dab419fd873bd6c8dfb1
morozov <81422715+fracex@users.noreply.github.com>
2025-12-03 19:23:18 +0300

    Add files via upload

---
+++ n2.3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
+int main() {
+int a,x;
+cout<<"Введите число a:\n";cin>>a;
+cout<<"Введите число x:\n";cin>>x;
+cout<<"a) "<<sqrt((2*a+sin(abs(3*a)))/3.56)<<"\n";
+cout<<"x) "<<sin((3.2+sqrt(1+x))/abs(5*x));
+}
