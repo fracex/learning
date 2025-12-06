83cd7b42850b225f5fcbbd6d1d5cfbf4187700ad
morozov <81422715+fracex@users.noreply.github.com>
2025-12-06 23:11:53 +0300

    Add files via upload

---
+++ n2.4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){
+    int a;
+    cout<<"Введите сторону квадрата:"<<endl;
+    cin>>a;
+    cout<<"Периметр квадрата = "<<a*a;
+}
