534db1af258cd644460718d573bd0c99e624d9e8
morozov <81422715+fracex@users.noreply.github.com>
2025-12-06 23:14:52 +0300

    Add files via upload

---
+++ n2.5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){
+    int a;
+    cout<<"Введите радиус окружности:"<<endl;
+    cin>>a;
+    cout<<"Диаметр окружности = "<<a*2;
+}
