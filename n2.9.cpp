c717259e9f6df2649e928f5dcb22a5f46f9f2278
morozov <81422715+fracex@users.noreply.github.com>
2025-12-09 22:06:36 +0300

    Add files via upload

---
+++ n2.9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int x,y,a,b;
+	cout<<"Введите x,y:\n";
+	cin>>x; cin>>y;	
+	cout<<"z = "<<(2*pow(x,3))-(3,44*x*y)+(2,3*pow(x,2))-(7,1*y)+2; cout<<"\n";
+	cout<<"Введите a,b:\n";
+	cin>>a; cin>>b;
+	cout<<"z = "<<(3,14*pow((a+b),3))+(2,75*pow(b,2))-(12,7*a)-4,1;
+}
