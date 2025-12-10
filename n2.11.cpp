0f39776dc9fa8511f2e2bd5e5f227c9f75cd94cd
morozov <81422715+fracex@users.noreply.github.com>
2025-12-10 23:13:40 +0300

    Add files via upload

---
+++ n2.11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int a,b;
+	cout<<"Введите объем тела:";
+	cin>>a; cout<<"Введите массу тела:";
+	cin>>b; cout<<"Плотность тела = "<<b/a<<endl;
+}
