02c497fcebcbc37c1bf687021b0e51f5e9365d54
morozov <81422715+fracex@users.noreply.github.com>
2025-12-17 15:40:13 +0300

    Add files via upload

---
+++ n2.12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"Введите площадь государства( в км²):"<<endl;
+    cin>>a;
+    cout<<"Введите кол-во населения:"<<endl;
+    cin>>b; "\n";
+    cout<<"Плотность населения:"<<b/a<<endl;
+}
