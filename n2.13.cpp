63d68d12fc07513110b1a22f484a98fe3c6e099f
morozov <81422715+fracex@users.noreply.github.com>
2025-12-17 15:48:34 +0300

    Add files via upload

---
+++ n2.13.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"ax+b=0"<<endl;
+    cout<<"Введите а, b:"<<endl;
+    cin>>a; cin>>b;
+    cout<<a<<"x+"<<b<<"=0 \n";
+    cout<<"x=-b/a \t"<<"x="<<-b/a<<endl;
+}
