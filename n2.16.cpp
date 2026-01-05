94e8e94fad15a7e376731dd54c098e6a803ba53d
morozov <81422715+fracex@users.noreply.github.com>
2026-01-06 00:27:09 +0300

    Add files via upload

---
+++ n2.16.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	setlocale(LC_ALL, "rus");
+	wcout << L"Введите катеты a, b:" << endl;
+	cin >> a; wcout << L"\n";
+	cin >> b; wcout << L"\n";
+	wcout << L"Периметр равен: " << sqrt(pow(a, 2) + pow(b, 2)) + a + b;
+}
